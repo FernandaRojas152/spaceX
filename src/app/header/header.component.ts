662cd0e6ff5c4e1a6e6afc70680bc51de20d5e4e
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2023-02-10 17:19:31 -0500

    connection to the api info succesful
    Added management of Errors on connection to api. handleError it's based on tour of heroes method.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { InfoSpaceX } from '../info-space-x';
+import { LaunchesService } from '../launches.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  info$: Observable<InfoSpaceX>;
+
+  constructor(private launchesService: LaunchesService){
+    this.info$= this.launchesService.getInfo();
+
+  }
+  ngOnInit(): void {
+    
+  }
 
 }
--- src/app/launches.service.ts
+++ src/app/launches.service.ts
@@ -1,30 +1,42 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
+import { Observable, throwError, of } from 'rxjs';
 import { catchError, retry, map } from 'rxjs/operators';
 import { InfoSpaceX } from './info-space-x';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LaunchesService implements OnInit{
-  private infoURL= "https://api.spacexdata.com/v3/info";
-  private launchesURL= "https://api.spacexdata.com/v3/launches";
+export class LaunchesService implements OnInit {
+  private infoURL = "https://api.spacexdata.com/v3/info";
+  private launchesURL = "https://api.spacexdata.com/v3/launches";
 
-  constructor(private http: HttpClient){
-
-  };
+  constructor(private http: HttpClient) { };
 
   ngOnInit(): void {
-    
+
   }
 
-  getInfo(): Observable<InfoSpaceX>{
-    return this.http.get<InfoSpaceX>(this.infoURL);
+  getInfo(): Observable<InfoSpaceX> {
+    return this.http.get<InfoSpaceX>(this.infoURL).pipe(
+      catchError(this.handleError<InfoSpaceX>("GetInfo")),
+    );
   }
 
-  getLaunches(){
+  getLaunches() {
     //change to type observable later.
     this.http.get(this.launchesURL);
+  }
+
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: HttpErrorResponse): Observable<T> => {
+      if (error.status == 0) {
+        console.log(error.message);
+      } else {
+        console.log(error.error);
+      }
+
+      return of(result as T);
+    }
   }
 }
