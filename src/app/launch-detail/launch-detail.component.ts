2b41076b0bb827c651d8bd71187205f533e638b2
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2023-02-24 23:36:17 -0500

    reactive forms concept

--- src/app/launch-detail/launch-detail.component.ts
+++ src/app/launch-detail/launch-detail.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { LaunchesService } from '../launches.service';
 import { Observable, pipe, map } from 'rxjs';
 import { LaunchSpaceX } from '../launch-space-x';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-launch-detail',
   templateUrl: './launch-detail.component.html',
   styleUrls: ['./launch-detail.component.scss']
 })
-export class LaunchDetailComponent implements OnInit{
+export class LaunchDetailComponent implements OnInit {
   launch$: Observable<LaunchSpaceX>;
   id: number;
-  form: FormGroup;
+  form: FormGroup= new FormGroup({
+    mission_name:new FormControl(''),
+    launch_year: new FormControl(''),
+    rocket_name: new FormControl(''),
+    site_name: new FormControl(''),
+    details: new FormControl(''),
+
+  });
 
   constructor(private route: ActivatedRoute, private launchesService: LaunchesService, private location: Location) {
   }
@@ -27,5 +34,9 @@ export class LaunchDetailComponent implements OnInit{
     const id = +this.route.snapshot.paramMap.get('id')!;
     this.id = id;
     this.launch$ = this.launchesService.getLaunch(id);
+  }
+
+  saveLaunch(){
+
   }
 }
