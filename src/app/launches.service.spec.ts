5c3d6197b6f54eb482e972471d5df04c181d7fd2
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2023-03-29 23:52:21 -0500

    adding update launch and modifying launch-space interface

--- src/app/launches.service.spec.ts
+++ src/app/launches.service.spec.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
+import { fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
+import { of, throwError } from 'rxjs';
 import { InfoSpaceX } from './info-space-x';
+import { LaunchSpaceX } from './launch-space-x';
 
 import { LaunchesService } from './launches.service';
 
@@ -19,7 +20,7 @@ describe('LaunchesService', () => {
     httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
     /* service = TestBed.inject(LaunchesService);
     httpMock = TestBed.inject(HttpTestingController); */
-    service= new LaunchesService(httpClientSpy);
+    service = new LaunchesService(httpClientSpy);
   });
 
   /* afterEach(() => {
@@ -30,51 +31,136 @@ describe('LaunchesService', () => {
     expect(service).toBeTruthy();
   });
 
-  
-  describe('IsFavorite', ()=>{
-    it(`should be true if the id is favorite`, ()=>{
-      const favoriteId= 2;
-      service.favorite= favoriteId;
-      const results= service.isFavorite(favoriteId);
+
+  describe('IsFavorite', () => {
+    it(`should be true if the id is favorite`, () => {
+      const favoriteId = 2;
+      service.favorite = favoriteId;
+      const results = service.isFavorite(favoriteId);
       expect(results).toBeTrue();
     });
 
-    it(`should be false if the id is favorite`, ()=>{
-      const favoriteId= 2;
-      service.favorite= favoriteId;
-      const results= service.isFavorite(9);
+    it(`should be false if the id is favorite`, () => {
+      const favoriteId = 2;
+      service.favorite = favoriteId;
+      const results = service.isFavorite(9);
       expect(results).toBeFalse();
     });
   });
 
-  describe('addFavorite', ()=>{
-    it('should add the favorite if the id is correct', ()=>{
-      const favoriteId= 2;
+  describe('addFavorite', () => {
+    it('should add the favorite if the id is correct', () => {
+      const favoriteId = 2;
       service.addFavorite(favoriteId);
       expect(service.favorite).toEqual(favoriteId);
     })
-    it(`shouldn't add the favorite if the id is not valid`, ()=>{
+    it(`shouldn't add the favorite if the id is not valid`, () => {
       service.addFavorite(null);
       expect(service.favorite).toBeNull();
     })
   });
 
-  describe('getInfo', ()=>{
-    it('should retrieve info from the API', (done: DoneFn) => {
+  describe('getInfo', () => {
+    it('should retrieve info from the API', fakeAsync(() => {
       const mockInfo: InfoSpaceX = {
         name: 'SpaceX',
         founder: 'Elon Musk',
         summary: 'SpaceX designs, manufactures and launches advanced rockets and spacecraft. The company was founded in 2002 to revolutionize space technology, with the ultimate goal of enabling people to live on other planets.'
       };
       httpClientSpy.get.and.returnValue(of(mockInfo));
-      service.getInfo().subscribe(data=> {
+      service.getInfo().subscribe(data => {
         expect(data).toEqual(mockInfo);
-        done();
       });
-    });
+    }));
   });
 
-/*   describe('getLaunches', ()=>{
+  describe('getLaunch', () => {
+    it('should retrieve one launch from the API if the number exists', fakeAsync(() => {
+      const id = 2;
+      const mockLaunch: LaunchSpaceX = {
+        "flight_number": 2,
+        "mission_name": "DemoSat",
+        "launch_year": "2007",
+        "rocket": {
+          "rocket_name": "Falcon 1",
+        },
+        "launch_site": {
+          "site_name": "Kwajalein Atoll",
+        },
+        "links": {
+          "mission_patch": "https://images2.imgbox.com/be/e7/iNqsqVYM_o.png",
+          "mission_patch_small": "https://images2.imgbox.com/4f/e3/I0lkuJ2e_o.png",
+          "youtube_id": "Lk4zQ2wP-Nc",
+        },
+        "details": "Successful first stage burn and transition to second stage, maximum altitude 289 km, Premature engine shutdown at T+7 min 30 s, Failed to reach orbit, Failed to recover first stage",
+      };
 
-  }); */
+      httpClientSpy.get.and.returnValue(of(mockLaunch));
+      service.getLaunch(id).subscribe(launch => {
+        expect(launch).toEqual(mockLaunch);
+      });
+    }));
+
+    it('shouldn\'t retrieve a launch if the id doesn\'t exists', fakeAsync(() => {
+      const id = 999;
+      const mockError = new HttpErrorResponse({
+        error: 'Not Found',
+        status: 404, statusText: 'Not Found'
+      });
+      httpClientSpy.get.and.returnValue(throwError(() => mockError));
+      service.getLaunch(id).subscribe({
+        error: error => {
+          expect(error.message).toContain('Not Found');
+          expect(error.status).toEqual(404);
+        }
+      });
+    }));
+  });
+
+  describe('updateLaunch', () => {
+    it('should update the launch if the id exists', fakeAsync(() => {
+      const id = 2;
+      const mockLaunch: LaunchSpaceX[] = [{
+        "flight_number": 2,
+        "mission_name": "DemoSat",
+        "launch_year": "2007",
+        "rocket": {
+          "rocket_name": "Falcon 1",
+        },
+        "launch_site": {
+          "site_name": "Kwajalein Atoll",
+        },
+        "links": {
+          "mission_patch": "https://images2.imgbox.com/be/e7/iNqsqVYM_o.png",
+          "mission_patch_small": "https://images2.imgbox.com/4f/e3/I0lkuJ2e_o.png",
+          "youtube_id": "Lk4zQ2wP-Nc",
+        },
+        "details": "Successful first stage burn and transition to second stage, maximum altitude 289 km, Premature engine shutdown at T+7 min 30 s, Failed to reach orbit, Failed to recover first stage",
+      }];
+      const expectedLaunch: LaunchSpaceX = {
+        "flight_number": 2,
+        "mission_name": "Fer",
+        "launch_year": "2001",
+        "rocket": {
+          "rocket_name": "Falcon 1",
+        },
+        "launch_site": {
+          "site_name": "Kwajalein Atoll",
+        },
+        "links": {
+          "mission_patch": "https://images2.imgbox.com/be/e7/iNqsqVYM_o.png",
+          "mission_patch_small": "https://images2.imgbox.com/4f/e3/I0lkuJ2e_o.png",
+          "youtube_id": "Lk4zQ2wP-Nc",
+        },
+        "details": "Successful first stage burn and transition to second stage, maximum altitude 289 km, Premature engine shutdown at T+7 min 30 s, Failed to reach orbit, Failed to recover first stage",
+      };
+      service.launches= mockLaunch;
+      service.updateLaunch(id, expectedLaunch);
+      expect(service.launches[0].mission_name).toEqual(expectedLaunch.mission_name);
+    }));
+  })
+
+  /*   describe('getLaunches', ()=>{
+  
+    }); */
 });
